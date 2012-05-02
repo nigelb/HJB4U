b87fb9023b65c9c3736f36ac59e69928903a9f0c
nigelb <nigel.blair@gmail.com>
2012-05-02 17:08:54 +1000

    Fixed scripts.

--- HJB4U/src/main/bin/createxml.bat
+++ HJB4U/src/main/bin/createxml.bat
@@ -1,9 +1,9 @@
 @echo off
 setLocal EnableDelayedExpansion
 set CLASSPATH="
-for /R ./lib %%a in (*.jar) do (
+for /R ../java %%a in (*.jar) do (
 set CLASSPATH=!CLASSPATH!;%%a
 )
 set CLASSPATH=!CLASSPATH!"
 
-java -cp %~dp0/../java/* hjb4u.launch.CLCreateXML %1 %2 %3 %4 %5 %6 %7 %8 %9
+java hjb4u.launch.CLCreateXML %1 %2 %3 %4 %5 %6 %7 %8 %9
--- HJB4U/src/main/bin/launch.bat
+++ HJB4U/src/main/bin/launch.bat
@@ -1,9 +1,9 @@
 @echo off
 setLocal EnableDelayedExpansion
 set CLASSPATH="
-for /R ./lib %%a in (*.jar) do (
+for /R ../java %%a in (*.jar) do (
 set CLASSPATH=!CLASSPATH!;%%a
 )
 set CLASSPATH=!CLASSPATH!"
 
-java -cp %~dp0/../java/* hjb4u.launch.Launch
+java hjb4u.launch.Launch
