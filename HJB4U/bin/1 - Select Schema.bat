f35a34731a746d56521c5318c245e560b2319fbb
nigelb <nigel.blair@gmail.com>
2014-07-22 07:54:33 +1000

    Remove old bin directory

--- HJB4U/bin/1 - Select Schema.bat
+++
@@ -1,2 +0,0 @@
-java -jar schema-selector-0.0.1.jar ../src/main/resources/hjb4u/conf/schema.properties
-pause
--- HJB4U/bin/1 - Select Schema.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd bin 2> /dev/null
-java -jar schema-selector-0.0.1.jar ../src/main/resources/hjb4u/conf/schema.properties
